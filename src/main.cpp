ae2d537ce83f540f78d97ab7c82f65ee8d558e3e
andrewcgaitskell <andrew@gaitskell.com>
2019-12-18 13:41:48 +0000

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -205,7 +205,7 @@ void loop() {
   //////////
   
   // Fill along the length of the strip in various colors...
-  colorWipe(strip.Color(255,   0,   0), 50); // Red
+  // colorWipe(strip.Color(255,   0,   0), 50); // Red
   //colorWipe(strip.Color(  0, 255,   0), 50); // Green
   //colorWipe(strip.Color(  0,   0, 255), 50); // Blue
 
